4ec541eb7411bd3da72e66720d533d488a7f3063
Ananay-22 <guptavananay@gmail.com>
2022-12-26 16:11:24 -0500

    remove DPRINT in main, using printf

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ ICM_20948 icm(p28, p27);
 // main() runs in its own thread in the OS
 int main()
 {    
-    DPRINT("Connected to mbed\r\n");    
-    DPRINT("WHO_AM_I: %x\r\n", icm . whoami());
+    printf("Connected to mbed\r\n");    
+    printf("WHO_AM_I: %x\r\n", icm . whoami());
 
     if (!icm . isConnected()) {
-        DPRINT("Cannot detect ICM 20948\r\n");
+        printf("Cannot detect ICM 20948\r\n");
         return 1;
     }
     
